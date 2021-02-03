081fc57248dbe2544dbbf18f1ef48bcce65ce765
Jay-Are Ocero <199ocero@gmail.com>
2021-02-04 04:43:58 +0800

    update changes

--- routes/botman.php
+++ routes/botman.php
@@ -3,15 +3,15 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
-$botman->hears('{keyword}', function ($bot,$keyword) {
+$botman->hears('Hi', function ($bot) {
 
 
-    $keywordFind = str_replace(" ","%20",$keyword);
+    // $keywordFind = str_replace(" ","%20",$keyword);
     
-    $getWikiAnswer =json_decode(file_get_contents('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=1&explaintext=1&redirects=1&titles='.$keywordFind.'&indexpageids'), true);
-    $pageid = $getWikiAnswer['query']['pageids'][0];
-    $getAnswer = $getWikiAnswer['query']['pages'][$pageid]['extract'];
+    // $getWikiAnswer =json_decode(file_get_contents('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=1&explaintext=1&redirects=1&titles='.$keywordFind.'&indexpageids'), true);
+    // $pageid = $getWikiAnswer['query']['pageids'][0];
+    // $getAnswer = $getWikiAnswer['query']['pages'][$pageid]['extract'];
 
-    $bot->reply($getAnswer);
+    $bot->reply('Hello');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
