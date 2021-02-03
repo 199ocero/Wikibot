2c6afc9a3842c86f5e9104b4e3bd2073cf1e6928
Jay-Are Ocero <199ocero@gmail.com>
2021-02-04 04:28:24 +0800

    update changes

--- routes/botman.php
+++ routes/botman.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('{keyword}', function ($bot,$keyword) {
-    $bot->reply('Hello! You write: '.$keyword);
+
+
+    $keywordFind = str_replace(" ","%20",$keyword);
+    
+    $getWikiAnswer =json_decode(file_get_contents('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=1&explaintext=1&redirects=1&titles='.$keywordFind.'&indexpageids'), true);
+    $pageid = $getWikiAnswer['query']['pageids'][0];
+    $getAnswer = $getWikiAnswer['query']['pages'][$pageid]['extract'];
+
+    $bot->reply($getAnswer);
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
